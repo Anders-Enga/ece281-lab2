44fdb96dd701b3ae1e078cf843fbf7613c9d8338
Anders-Enga <177686919+Anders-Enga@users.noreply.github.com>
2025-03-02 17:16:36 -0700

    Completed VHDL

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -32,13 +32,38 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder_tb is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder_tb;
 
 architecture Behavioral of sevenseg_decoder_tb is
 
+    component sevenseg_decoder is
+        port(
+            i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+            o_seg_n : out STD_LOGIC_VECTOR (6 downto 0)
+        );	
+    end component;
+
+  signal w_Hex : std_logic_vector (3 downto 0):= (others=> '0');
+  signal w_seg : std_logic_vector (6 downto 0) := (others=> '0');
+  
+
 begin
 
-
+sevenseg_decoder_inst : sevenseg_decoder port map (
+    i_Hex => w_Hex,
+    o_seg_n => w_seg
+   );
+   test_process : process
+   begin
+   
+        w_Hex <= x"0"; wait for 10 ns;
+            assert w_seg = "1000000" report "error on x0" severity failure;
+        w_Hex <= x"8"; wait for 10 ns;
+            assert w_seg = "0000000" report "error on x8" severity failure;
+        w_Hex <= x"A"; wait for 10 ns;
+            assert w_seg = "0001000" report "error on xA" severity failure;
+        
+        wait; -- wait forever
+   end process;
+   
 end Behavioral;
